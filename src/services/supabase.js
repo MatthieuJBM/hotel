faa27a3fa5e10ba7006a6ee476f0db86406e239e
Mateusz Baryla <m.baryla@protonmail.ch>
2023-09-01 18:48:39 +0200

    setting up supabase, connection between react and supabase implemented

---
+++ src/services/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://ftlfpzomxetqtrfrjxnz.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ0bGZwem9teGV0cXRyZnJqeG56Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTM1ODEzNDksImV4cCI6MjAwOTE1NzM0OX0.umnfHgpcBhxQO3xKoiV3tu6rXD2LIwO7ITwCfzwz-3s";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
