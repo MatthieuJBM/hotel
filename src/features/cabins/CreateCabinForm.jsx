fa3ca597addc959d32d354e67ddd67522153adc1
Mateusz Baryla <m.baryla@protonmail.ch>
2023-09-07 19:03:42 +0200

    Images can be uploaded to Supabase

--- src/features/cabins/CreateCabinForm.jsx
+++ src/features/cabins/CreateCabinForm.jsx
@@ -66,7 +66,8 @@ function CreateCabinForm() {
   });
 
   function onSubmit(data) {
-    mutate(data);
+    //mutate(data);
+    mutate({ ...data, image: data.image[0] });
   }
 
   function onError(errors) {}
@@ -85,17 +86,6 @@ function CreateCabinForm() {
    
    */
     <Form onSubmit={handleSubmit(onSubmit, onError)}>
-      {/* <FormRow>
-        <Label htmlFor="name">Cabin name</Label>
-        <Input
-          type="text"
-          id="name"
-          {...register("name", {
-            required: "This field is required.",
-          })}
-        />
-        {errors?.name?.message && <Error>{errors.name.message}</Error>}
-      </FormRow> */}
       <FormRow label="Cabin name" error={errors?.name?.message}>
         <Input
           type="text"
@@ -168,7 +158,13 @@ function CreateCabinForm() {
       </FormRow>
 
       <FormRow label="Cabin photo">
-        <FileInput id="image" accept="image/*" />
+        <FileInput
+          id="image"
+          accept="image/*"
+          {...register("image", {
+            required: "This field is required",
+          })}
+        />
       </FormRow>
 
       <FormRow>
--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -1,4 +1,4 @@
-import supabase from "./supabase";
+import supabase, { supabaseUrl } from "./supabase";
 
 export async function getCabins() {
   let { data, error } = await supabase.from("cabins").select("*");
@@ -12,13 +12,45 @@ export async function getCabins() {
 }
 
 export async function createCabin(newCabin) {
+  const imageName = `${Math.random()}-$(newCabin.image.name)`.replaceAll(
+    "/",
+    ""
+  );
+  const imagePath = `${supabaseUrl}/storage/v1/object/public/cabin-images/${imageName}`;
+
+  // https://ftlfpzomxetqtrfrjxnz.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg
+
+  // 1. Create cabin
   const { data, error } = await supabase
     .from("cabins")
-    .insert([newCabin])
+    .insert([{ ...newCabin, image: imagePath }])
     .select();
   if (error) {
     console.error(error);
     throw new Error("Cabin could not be created.");
+  }
+
+  // 2. Upload image
+  //  We need to specify the image path, that we want to create.
+
+  //const avatarFile = event.target.files[0];
+  const { error: storageError } = await supabase.storage
+    .from("cabin-images")
+    .upload(imageName, newCabin.image);
+  // , {
+  //   cacheControl: "3600",
+  //   upsert: false,
+  // });
+
+  // 3. Prevent a new cabin for being created in case in case
+  //    that this file didn't upload correctly.
+  // Delete the cabin IF there was an error uploading image.
+  if (storageError) {
+    await supabase.from("cabins").delete().eq("id", data.id);
+    console.error(storageError);
+    throw new Error(
+      "Cabin image could not be uploaded and the cabin was not created."
+    );
   }
   return data;
 }
